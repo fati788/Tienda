994b694c2e8abd4e2ae0218a46125f455dac1d69
fati788 <fela102@g.educaand.es>
2025-04-21 21:07:40 +0200

    clases documentadas

--- src/Producto.java
+++ src/Producto.java
@@ -1,42 +1,85 @@
-public class Producto {
+/**
+  * La clase Producto representa un artículo disponible en una tienda. Contiene información
+  * sobre su nombre, precio y cantidad en stock.
+  * @version 1.0
+  * @author fatima
+  */
 
+public class Producto {
+ 
 	private String nombre;
 	private double precio;
 	private int stock;
 	
+	/**
+	 * Constructor con todos los parametros del producto
+	 * @param nombre es un string que representa el nombre del producto
+	 * @param precio es un double que representa el precio del producto
+	 * @param stock es un integer que representa el cantidad del stock
+	 */
 	public Producto (String nombre, double precio, int stock) {
 		this.nombre=nombre;
 		this.precio=precio;
 		this.stock=stock;
 	}
+	/**
+	 * Devuelve el nombre del producto
+	 * @return Devuelve el nombre del producto
+	 */
 	
 	public String getNombre() {
 		return nombre;
 	}
+	/**
+	 * Establece un nuevo nombre para el producto
+	 * @param nombre es un string que representa el nombre del producto
+	 */
 	
 	public void setNombre (String nombre) {
 		this.nombre=nombre;
 	}
+	/**
+	 * Devuelve el precio del producto
+	 * @return devuelve el precio del producto
+	 */
 	
 	public double getPrecio () {
 		return precio;
 	}
+	/**
+	 * Establece un nuevo precio para el producto
+	 * @param precio es un double que representa el precio del producto
+	 */
 	
 	public void setPrecio (double precio) {
 		this.precio=precio;
 	}
+	/**
+	 * Devuelve la cantidad en stock del producto
+	 * @return devuelve el stock del producto
+	 */
 	
 	public int getStock() {
 		return stock;
 	}
-	
+	/**
+	 * Establece una nueva cantidad en stock para el producto
+	 * @param stock  es un integer que representa el cantidad del stock
+	 */
 	public void setStock(int stock) {
 		this.stock=stock;
 	}
-	
+	/**
+	 * Reduce el stock del producto en una cantidad determinada
+	 * @param cantidad es la cantidad que se resta del stock
+	 */
 	public void reducirStock (int cantidad) {
 		this.stock-=cantidad;
 	}
+	/**
+	 * Aumenta el stock del producto en una cantidad determinada
+	 * @param cantidad es la cantidad que se suma al stock
+	 */
 	
 	public void aumentarStock (int cantidad) {
 		this.stock+=cantidad;
--- src/Tienda.java
+++ src/Tienda.java
@@ -1,21 +1,44 @@
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * La clase tienda gestiona un inventario de productos 
+ * que nos permitir agregar productos , venderlos aplicando
+ * descuentos , actualizar precios stock , y calcular el valor 
+ * del total inventario
+ * @version 1.0
+ * @author fatima
+ */
 public class Tienda {
 	private static final double DESCUENTO_25_PORCIENTO = 0.25;
 	private static final double DESCUENTO_10_PORCIENTO = 0.10;
 	private List <Producto> productos;
-	
+	/**
+	 * un contruclor que no recibe nada 
+	 * es solo inicializar el arrayList del productos.
+	 */
 	public Tienda() {
 		productos =new ArrayList<>();
 	}
-	
+	/**
+	 * Este método crea un objeto de tipo Producto con el nombre, precio y stock
+	 * proporcionados, y lo agrega a la lista productos del inventario
+	 * @param nombre es un string que representa el nombre del producto
+	 * @param precio es un double que representa el precio del producto
+	 * @param stock es un integer que representa el cantidad del stock
+	 */
 	// Añadir producto nuevo
 	public void agregarProducto (String nombre, double precio, int stock) {
 		Producto producto= new Producto (nombre, precio, stock);
 		productos.add(producto);
 	}
-	
+	/**
+	 * Realiza una venta de un producto. Si se encuentra el producto en el inventario y hay
+	 * suficiente stock, aplica un descuento dependiendo del precio total de la venta.
+	 * Después, reduce el stock del producto y muestra el subtotal, el descuento aplicado y el
+	 * total con descuento.
+	 * @param nombreProducto es el nombre del producto
+	 * @param totalVenta es la cantidad del dinero total  del total del venta 
+	 */
 	// Vender producto
 	public void venderProducto(String nombreProducto, int totalVenta) {
 	    boolean productoEncontrado = false;  // Variable para verificar si se encontró el producto
@@ -60,6 +83,13 @@ public class Tienda {
 	        System.out.println("Producto "+nombreProducto +" no encontrado.");
 	    }
 	}
+	/**
+	 * Mostra por pantalla los datos de la venta del producto 
+	 * @param producto es el producto del venta 
+	 * @param total representa el precio total 
+	 * @param descuento es el descuento que se hace sobre el precio del producto
+	 * @param totalConDescuento es el total del descuento
+	 */
 
 	private void venderProducto(Producto producto, double total, double descuento, double totalConDescuento) {
 		System.out.println("Venta "+ producto.getNombre()+" realizada. ");
@@ -67,18 +97,34 @@ public class Tienda {
 		System.out.println("Decuento aplicado: "+descuento);
 		System.out.println("Total : " + totalConDescuento);
 	}
-
+      /**
+       * Calcula el precio final despuis el descuento
+       * @param total es el precio total del venta
+       * @param descuento es descuento que se aplica sobre el precio
+       * @return devuelve el precio final del venta despuis el descuento
+       */
 	private double CalcularDescuentoVenta(double total, double descuento) {
 		double totalConDescuento = total - (total * descuento);
 		return totalConDescuento;
 	}
+	/**
+	 * Muestra por pantalla todos los productos en el inventario 
+	 * junto con su precio y stockactual.
+	 */
 
 	public void mostrarInventario() {
         for (Producto producto : productos) {
             System.out.println(producto.getNombre() + " - Precio: " + producto.getPrecio() + " - Stock: " + producto.getStock());
         }
     }
-	
+	/**
+	 * Busca un producto por su nombre (ignorando mayúsculas/minúsculas)
+	 * Devuelve el objeto Producto correspondiente si se encuentra,
+	 *  o null si no se encuentra.
+	 * @param nombre es el nombre del producto que reciba el metodo
+	 * @return duevelve el producto si lo encuentra
+	 * si no devuelve un null
+	 */
 	
 	 // Buscar producto por nombre
     public Producto buscarProducto(String nombre) {
@@ -89,6 +135,11 @@ public class Tienda {
         }
         return null;
     }
+    /**
+     * Actualiza el precio de un producto si se encuentra en el inventario.
+     * @param nombre es el nombre del producto que se actualiza
+     * @param nuevoPrecio es el precio nuevo del producto
+     */
 	
     // Actualizar precio de un producto
     public void actualizarPrecio(String nombre, double nuevoPrecio) {
@@ -100,7 +151,11 @@ public class Tienda {
             System.out.println("Producto no encontrado.");
         }
     }
-	
+	/**
+	 * Aumenta el stock de un producto específico si se encuentra en el inventario.
+	 * @param nombre es el nombre del producto que vas a aumentar el stock de el
+	 * @param cantidad es la cantidad que sume a la cantidad antigua del precio
+	 */
     // Actualizar stock
     public void reponerStock(String nombre, int cantidad) {
         Producto producto = buscarProducto(nombre);
@@ -111,7 +166,11 @@ public class Tienda {
             System.out.println("Producto no encontrado.");
         }
     }
-	
+	/**
+	 * Calcula el valor total del inventario multiplicando el precio de cada producto por su
+	 * stock y sumando los resultados
+	 * @return devuelve el precio total del stock despuis calcular al valor del inventario
+	 */
     // Calcular valor total del inventario
     public double calcularValorInventario() {
         double precioTotalStock = 0;
